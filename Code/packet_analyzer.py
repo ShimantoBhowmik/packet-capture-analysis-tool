78c500d92f8d63b753b205ec2a971176c1ac3c19
Shimanto Bhowmik <bmikshimanto@gmail.com>
2023-11-04 11:40:03 -0400

    Fixed indentation on packet_parser and added initial code for packet_analyzer

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -4,6 +4,12 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter()
-parse()
-compute()
+def main():
+    L1,L2,L3,L4 = [],[],[],[]
+    filter()
+    for i in range(1,5):
+        parse("Node_" + str(i) + "_filtered.txt", eval("L" + str(i)))
+    compute()
+    
+if __name__ == "__main__":
+    main()
--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def parse(filename, L) :
-    
-	
-
-	
+def parse(filename, L):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            L.append(line.strip().split())
